759e36bd3850a30868ce0f0db61002918421aed4
yourname <yourname@site.com>
2019-12-21 21:05:38 +0500

    Some frontend features

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
 const express = require('express');
-// создаем объект приложения
 const app = express();
 const port = process.env.PORT || 80;
-// определяем обработчик для маршрута "/"
-app.get("/", function(request, response){
 
+app.get("/", function(request, response){
     response.sendfile("public/index.html");
 });
-app.get("/event", function(request, response){
 
+app.get("/event", function(request, response){
     response.sendfile("public/event.html");
 });
 
-
-
 app.use(express.static('public'));
-// начинаем прослушивать подключения на 3000 порту
+
 const server = app.listen(port, (error) => {
     if (error) return console.log(`Error: ${error}`);
 
