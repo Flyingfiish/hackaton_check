045cc71fc26be9c7be57c118b331af4531bf499e
yourname <yourname@site.com>
2019-12-21 17:12:06 +0500

    Init

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+// создаем объект приложения
+const app = express();
+const port = 3000;
+
+// определяем обработчик для маршрута "/"
+app.get("/", function(request, response){
+
+    response.sendfile("public/index.html");
+});
+app.get("/event", function(request, response){
+
+    response.sendfile("public/event.html");
+});
+app.use(express.static('public'));
+// начинаем прослушивать подключения на 3000 порту
+const server = app.listen(port, (error) => {
+    if (error) return console.log(`Error: ${error}`);
+
+    console.log(`Server listening on port ${server.address().port}`);
+});
---
+++ public/script.js
@@ -0,0 +1,36 @@
+function GetRandomCode() {
+    let elem = document.getElementById('code');
+    let id = Math.floor(Math.random() * (9999 - 1000)) + 1000;
+
+    elem.textContent = id;
+    let event = {
+        id: id,
+        history: [
+            {
+                emoji: 'hmm',
+                time: new Date().getTime()
+            }
+        ]
+    };
+    events.push(event);
+}
+
+let events = [];
+
+function OnClickButton(string){
+
+    let elem1 = document.getElementById('history');
+    let div = document.createElement('div');
+    let image = document.createElement('img');
+    let time = document.createElement('label');
+    let date = new Date();
+    time.textContent = date.getHours().toString() + ':' + date.getMinutes().toString() + ':' + date.getSeconds().toString() + '  ';
+    elem1.scrollTop = 9999;
+    image.src = 'emoji/' + string + '.jpg';
+    div.appendChild(time);
+    div.appendChild(image);
+    elem1.appendChild(div);
+}
+
+
+
