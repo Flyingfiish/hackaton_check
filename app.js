0be52359cf86f13a684dfae718e24fd7bcf94d2b
Eldos <eldarg76@gmail.com>
2019-12-23 20:21:09 +0500

    not working css

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
-app.get("/", function(request, response){
-    response.sendfile("public/index.html");
+app.set("view engine", "hbs");
+ 
+app.use("/", function(request, response){
+     
+    response.render("layouts/layout.hbs");
 });
 
 app.get("/event", function(request, response){
     response.sendfile("public/event.html");
 });
 
-app.use(express.static('public'));
+app.use(express.static("static"));
 
 const server = app.listen(port, (error) => {
     if (error) return console.log(`Error: ${error}`);
